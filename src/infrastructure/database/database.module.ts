049fe3363212a5214f6648903689c68a5a14fdde
yoannclaisse <claisse.yoann@gmail.com>
2024-04-09 13:11:51 +0200

    comment loggin option

--- src/infrastructure/database/database.module.ts
+++ src/infrastructure/database/database.module.ts
@@ -5,25 +5,27 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    imports: [
-      TypeOrmModule.forRootAsync({
-        imports: [ConfigModule],
-        inject: [ConfigService],
-        useFactory: (configService: ConfigService) => ({
-          type: 'postgres',
-          host: configService.get('DATABASE_HOST'),
-          port: configService.get('DATABASE_PORT'),
-          username: configService.get('DATABASE_USER'),
-          password: configService.get('DATABASE_PASSWORD'),
-          database: configService.get('DATABASE_NAME'),
-          entities: [
-            // __dirname + '/../**/*.entity.ts',
-            User,
-            Task
-          ],
-          synchronize: true,
-        })
-      }),
-    ],
-  })
-export class DatabaseModule {}
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        entities: [
+          // __dirname + '/../**/*.entity.ts',
+          User,
+          Task
+        ],
+        synchronize: true,
+        autoLoadEntities: true,
+        // logging: true,
+      })
+    }),
+  ],
+})
+export class DatabaseModule { }
