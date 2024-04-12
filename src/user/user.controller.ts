59317e0b9ce1b991e81e90bf376312e24c04a4c2
yoannclaisse <claisse.yoann@gmail.com>
2024-04-12 11:31:59 +0200

    remove async

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,7 +5,7 @@ import { User } from '../../prisma/models/User';
 @Controller('')
 export class UserController {
 
-    constructor(private userService: UserService) {}
+    constructor(private userService: UserService) { }
 
     @Get(':email')
     getUserByEmail(@Param('email') email: string) {
@@ -18,20 +18,20 @@ export class UserController {
     }
 
     @Post()
-    async createUser(@Body() post: User) {
+    createUser(@Body() post: User) {
         const emailRegex = new RegExp("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
         if (!emailRegex.test(post.email)) {
             throw new HttpException('email is not valid', HttpStatus.BAD_REQUEST)
         }
-        const userEmail = await this.userService.getUser(post.email)
-        if (!!userEmail) {
-            throw new HttpException('User already exist', HttpStatus.CONFLICT)
-        }
-        // console.log(post)
-        return this.userService.addUser(post.email/*, post.username*/).catch(
-            error => {
-                throw new HttpException(error.message, HttpStatus.BAD_REQUEST)
+        return this.userService.getUser(post.email).then((user) => {
+            if (!!user) { 
+                throw new HttpException('User already exists', HttpStatus.CONFLICT) 
             }
-        )
+            return this.userService.addUser(post.email/*, post.username*/).catch(
+                error => {
+                    throw new HttpException(error.message, HttpStatus.BAD_REQUEST)
+                }
+            )
+        })
     }
 }
