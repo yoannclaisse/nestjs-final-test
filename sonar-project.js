a29777b7294d2c74fef31e477f8c53e1ea13e8ff
yoannclaisse <claisse.yoann@gmail.com>
2024-04-17 11:00:57 +0200

    fix sonar config

--- sonar-project.js
+++ sonar-project.js
@@ -5,12 +5,12 @@ sonarqubeScanner(
         serverUrl: 'http://localhost:9000',
         options: {
             'sonar.sources': 'src',
-            'sonar.tests': 'src',
+            'sonar.tests': 'test',
             'sonar.inclusions': 'src/**/*.ts', // Entry point of your code
             'sonar.test.inclusions':
-                'src/**/*.spec.ts,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx',
+                'test/**/*.spec.ts,test/**/*.e2e-spec.ts,test/**/*.spec.jsx,test/**/*.test.js,test/**/*.test.jsx',
             'sonar.login': 'sqa_47b2de3d4dff47889e58ee08a880d85bd5e009f7',
-            'sonar.javascript.lcov.reportPaths': 'test/coverage/lcov.info'
+            'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info'
         },
     },
     () => {
